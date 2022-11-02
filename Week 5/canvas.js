fad76e0512b67653b8424e2632e2403ff9ee46cc
Joshua Bholan <jbholan@email.nit.edu>
2022-11-02 13:46:17 -0400

    yes

---
+++ Week 5/canvas.js
@@ -0,0 +1,63 @@
+//singal line comment
+/*
+multiline comment
+*/
+//defines variable access by id
+var canvas = document.getElementById("canvas")
+//define drawing context of the Canvas Element
+var ctx = canvas.getContext('2d');
+
+//draw stuff
+//Sets up color and outline styles
+ctx.fillStyle = "rgb(0,0,255)";
+ctx.strokeStyle = "green";
+ctx.lineWidth = "5";
+//Draws a Rectangle fillRect(x,y,width,height)
+ctx.fillRect(30,30,200,100);
+ctx.strokeRect(300,30,200,100);
+
+//draw lines
+ctx.beginPath();
+ctx.moveTo(0,0);
+ctx.lineTo(400,250);
+ctx.lineTo(800,0);
+ctx.stroke();
+
+ctx.strokeStyle = "red";
+
+ctx.beginPath();
+ctx.moveTo(800,600);
+ctx.lineTo(400,350);
+ctx.lineTo(0,600);
+ctx.stroke();
+
+//draw circle
+ctx.beginPath();
+ctx.arc(400,300,50,0, (3*Math.PI)/2, false);
+ctx.lineTo(400,300);
+ctx.closePath();
+ctx.fill();
+ctx.stroke();
+
+//Some Random Shape
+ctx.fillStyle = "#55ddef";
+ctx.strokeStyle = "yellow";
+ctx.lineWidth = "2";
+
+ctx.beginPath();
+ctx.moveTo(650,100);
+ctx.lineTo(700,140);
+ctx.lineTo(675,200);
+ctx.lineTo(625,200);
+ctx.lineTo(600,140);
+ctx.closePath();
+ctx.fill();
+ctx.stroke();
+
+//Draw an image to the Canvas
+var mario = new Image();
+mario.src = "images/mario.png";
+
+mario.onload = function(){
+    ctx.drawImage(mario, 600,300, 40,80)
+}
