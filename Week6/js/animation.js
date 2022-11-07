cda404d8fe09dd2fd485e0df6a689f1692cc202f
Joshua Bholan <jbholan@email.nit.edu>
2022-11-07 13:04:13 -0500

    woah

--- Week6/js/animation.js
+++ Week6/js/animation.js
@@ -3,6 +3,7 @@ var ctx = canvas.getContext("2d");
 
 var timer = requestAnimationFrame(main);
 var x = 100;
+var speedX = 5;
 
 var mario = new Image();
 mario.src = "images/mario.png";
@@ -10,19 +11,27 @@ mario.onload = function(){
     main();
 }
 
+var bg = new Image();
+bg.src = "images/mariosupershow.jpg";
+bg.onload = function(){
+    main();
+}
 
 function main(){
-    ctx.clearRect(300,0,canvas.width,canvas.height);
-    ctx.fillStyle = "red";
-    ctx.beginPath();
-    ctx.arc(x,300, 10, 0, 2*Math.PI, true);
-    ctx.fill();
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    ctx.drawImage(bg,0,0,canvas.width,canvas.height);
+    //ctx.fillStyle = "red";
+    //ctx.beginPath();
+    //ctx.arc(x,300, 10, 0, 2*Math.PI, true);
+    //ctx.fill();
+
     //draw sprite image
-    ctx.drawImage(mario,x,300,)
-    x += 5;
-    if(x > 800 + 20){
-        x = 20;
+    ctx.drawImage(mario,x,300, 100, 110);
+    x += speedX;
+    if(x > canvas.width - 100 || x < 0){
+        speedX *= -1;
     }
+    console.log(speedX)
     timer = requestAnimationFrame(main);
 }
 
