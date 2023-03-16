3df1b5038289a406481b59d9ef699c415199764b
Joshua Bholan <jbholan@email.neit.edu>
2023-03-16 00:29:25 -0400

    fff

--- GDS126/GDS_126_Engine/js/Data/cave.js
+++ GDS126/GDS_126_Engine/js/Data/cave.js
@@ -2,18 +2,19 @@ var x=false;
 var caveData ={
 	info:{
 		layout:[
-			[0,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,2],
-			[0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,2],
-			[2,x,x,7,x,x,7,x,x,x,x,x,x,7,7,x,7,7,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,2],
-			[6,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,2],
-			[5,x,x,x,x,x,x,x,9,10,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,2],
-			[7,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,2],
-			[x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,2],
-			[x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x]
+			[7,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,7],
+			[0,5,0,5,0,5,0,5,0,5,0,5,0,5,0,5,0,5,0,5,0,5,0,5,0,5,0,5,0,5,0,5,0,5,0],
+			[7,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,7,x,x,x,x,x,x,x,x,x,x,x,x,x,x,7],
+			[7,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,7,x,x,x,x,x,x,x,x,x,x,x,x,x,x,7],
+			[7,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,7,x,x,x,x,x,x,x,x,x,x,x,x,x,x,7],
+			[7,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,7,x,x,x,x,x,x,x,x,x,x,x,x,x,x,7],
+			[7,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,7,x,x,x,x,x,x,x,x,x,x,x,x,x,x,7],
+			[7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7],
+			[7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7],
 			
 			
 		],
-		src:`images/Tile.png`,
+		src:`images/Tilefront.png`,
 	},
 	states:
 	[		
@@ -21,61 +22,61 @@ var caveData ={
 				fps:5,
 				cycle:false,
 				frames:[
-					{width:64, height:64, startX:0, startY:0}
+					{width:128, height:128, startX:0, startY:0}
 				]
 			},
 			{
 				fps:1,
 				cycle:false,
-				frames:[{width:64, height:64, startX:64, startY:0}]
+				frames:[{width:128, height:128, startX:128, startY:0}]
 			},
 			{
 				fps:1,
 				cycle:false,
-				frames:[{width:64, height:64, startX:128, startY:0}]
+				frames:[{width:128, height:128, startX:256, startY:0}]
 			},
 			{
 				fps:1,
 				cycle:false,
-				frames:[{width:64, height:64, startX:192, startY:0}]
+				frames:[{width:128, height:128, startX:384, startY:0}]
 			},
 			{
 				fps:1,
 				cycle:false,
-				frames:[{width:64, height:64, startX:256, startY:0}]
+				frames:[{width:128, height:128, startX:512, startY:0}]
 			},
 			{
 				fps:1,
 				cycle:false,
-				frames:[{width:64, height:64, startX:320, startY:0}]
+				frames:[{width:128, height:128, startX:320, startY:0}]
 			},
 			{
 				fps:1,
 				cycle:false,
-				frames:[{width:64, height:64, startX:384, startY:0}]
+				frames:[{width:128, height:128, startX:640, startY:0}]
 			},
 			{
 				fps:1,
 				cycle:false,
-				frames:[{width:64, height:64, startX:448, startY:0}]
+				frames:[{width:128, height:128, startX:768, startY:0}]
 			},
 			{
 				fps:1,
 				cycle:false,
-				frames:[{width:64, height:64, startX:512, startY:0}]
+				frames:[{width:128, height:128, startX:896, startY:0}]
 			}
 		]
 	}
 	var caveBackData ={
 		info:{
 			layout:[
-			[0,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
-			[x,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
-			[x,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
-			[x,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
-			[3,3,3,3,3,3,0,1,0,1,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+			[8,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5],
 			[3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
-			[3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,7,8,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+			[3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+			[3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+			[3,3,3,3,3,3,0,3,0,3,3,0,3,3,3,3,3,3,3,0,3,3,3,3,3,3,3,3,0,3,3,0,3,3,3],
+			[3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+			[3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
 			],
 			src:`images/Tile.png`,
 		},
@@ -85,10 +86,10 @@ var caveData ={
 		var caveHitData={
 			info:{
 				layout:[
-					[0,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,2],
-					[2,8,6,8,6,6,8,6,6,6,6,6,6,8,8,1,8,8,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,7],
-					[2,x,x,8,x,x,8,x,x,x,x,x,x,8,8,x,1,1,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,2],
-					[6,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,2],
+					[x,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,2],
+					[x,8,6,8,6,6,8,6,6,6,6,6,6,8,8,1,8,8,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,7],
+					[x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,2],
+					[x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,2],
 					[x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,2],
 					[x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,2],
 					[x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,2],
