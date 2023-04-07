1ec1ad7df100cc9d46eebde20622f666d679de7d
Joshua Bholan <jbholan@email.nit.edu>
2023-04-07 11:59:47 -0400

    j

--- gds137/level0/level0mb/Week 1/Week 1/js/week1_boundary_detection_bounce.js
+++ gds137/level0/level0mb/Week 1/Week 1/js/week1_boundary_detection_bounce.js
@@ -5,7 +5,7 @@ var context;
 var timer;
 var interval = 1000/60;
 var player;
-var ball = new Ball();
+var ball;
 
 
 	canvas = document.getElementById("canvas");
@@ -36,10 +36,4 @@ function animate()
 	//---------------------------------------------------
 	
 	player.draw();
-}
-function ball()
-{
-ball.x=canvas.width;
-ball.x-= //Some number of pixels
-ball.vx = -ball.vx;
 }
