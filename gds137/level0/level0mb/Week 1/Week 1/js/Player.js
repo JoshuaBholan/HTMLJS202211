57ac9d27d2580ab56c9039f5b5a001851ab0f9ab
Joshua Bholan <jbholan@email.nit.edu>
2023-04-14 12:03:27 -0400

    h

--- gds137/level0/level0mb/Week 1/Week 1/js/Player.js
+++ gds137/level0/level0mb/Week 1/Week 1/js/Player.js
@@ -29,20 +29,7 @@ function Player()
 			context.fill();
 		context.restore();
 		
-	}	
-	//This draws the player to the screen
-	this.drawRect = function()
-	{
-		context.save();
-			context.fillStyle = this.color;
-			context.translate(this.x, this.y);
-			context.fillRect((-this.width/2), (-this.height/2), this.width, this.height);
-		context.restore();
-		
-	}	
-	
-	
-		
+	}
 	
 	//This changes the player's position
 	this.move = function()
