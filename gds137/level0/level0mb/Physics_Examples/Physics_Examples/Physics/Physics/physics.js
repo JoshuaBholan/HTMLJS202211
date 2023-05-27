16a614ce18eeee03ff6e2898c83ce669644cb770
Joshua Bholan <jbholan@email.neit.edu>
2023-05-26 23:10:07 -0400

    d

--- gds137/level0/level0mb/Physics_Examples/Physics_Examples/Physics/Physics/physics.js
+++ gds137/level0/level0mb/Physics_Examples/Physics_Examples/Physics/Physics/physics.js
@@ -5,13 +5,11 @@ var context;
 var timer;
 var interval = 1000/60;
 var player;
-var Platform;
 
 //---------------Set Friction and Gravity-----------------
 var frictionX = .85;	
 var frictionY = .97;
 var gravity = 1;
-var score = 0;
 //--------------------------------------------------------
 
 
@@ -20,12 +18,7 @@ var score = 0;
 	context = canvas.getContext("2d");	
 	
 	player = new GameObject();
-	Platform = new GameObject((canvas.width-500), 730, 250, 40, "blue");
-	player.force = 6;
-	player.color = "magenta";
-	Platform.color = "cyan"
-	player.vx = 10;
-	player.vy = 0;
+	player.force = 2;
 	
 	timer = setInterval(animate, interval);
 
@@ -40,142 +33,8 @@ function animate()
 	//showGravity();
 	//showPixelLock();
 	//showBounce();
-	context.fillText("Score",60,45,50);
-	context.fillText(score,100,45,500);
-	context.save();
-	context.strokeStyle = "Black";
-	context.beginPath();
-	context.moveTo(Platform.x,Platform.y);
-	context.lineTo(player.x,player.y);
-	context.closePath();
-	context.lineWidth = 1;
-	context.stroke();
-	context.restore();
-	if (a)
-	{
-		Platform.x += -5;
-	}
-	if(d)
-	{
-		Platform.x +=5;
-	}
-	//right bounce
-	if(player.x > canvas.width - player.width/2)
-	{
-		player.vx = -player.vx;	
-		player.x = canvas.width - player.width/2;
-	}
-	//top bounce
-	if(player.y < player.height+750 - canvas.height)
-	{
-		player.vy = -player.vy;
-		player.y < player.height -canvas.height;
-	}
-	//left bounce
-	if(player.x < 50)
-	{
-		player.vx = -player.vx;
-		player < 50 - player.width/2;
-
-	}
 	
-	player.vy *= frictionY;
-	player.vx *= frictionX;
-	
-	player.vy += gravity;
-	
-	player.x += player.vx;
-	player.y += player.vy;
-	player.vx *= frictionX;	
-	player.vy *= frictionY;
-	//player.vy += gravity;
-	//player.y += player.vy;
-	
-	//player.vy *= frictionY;
-	//player.vx *= frictionX;
-	
-	//player.vy += gravity;
-	
-	//player.x += player.vx;
-	//player.y += player.vy;
-	//---------------------------------------------------------------------------------------
-	
-	//player.vx *= frictionX;
-	//player.x += player.vx;
-	//------Round the velocity before applying it to the position.--------------------------
-    //------This will keep the object from moving fractions of a pixel----------------------
-	//------This might not be noticeable now, but will help alot when things get complex----
-	player.y += Math.round(player.vy);
-	player.x += Math.round(player.vx);
-
-	
-	//--------------------Check Collision------------------------------------------------------
-	if(player.y > canvas.height - player.height/2)
-	{
-		
-		//--------Bounce the Ball---------------------------------------------------------------
-		player.y = canvas.height - player.height/2;
-		//the decimal is how bouncy you want the object to be
-		//It should be a number between 0 and 2;
-		player.vy = -player.vy * 1.2;
-		score = 0;
-		
-	}
-	if(player.hitTestObject(Platform))
-	{
-		score = score + 1;
-	}
-	if(player.hitTestObject(Platform))
-	{
-		//player.y = Platform.height - player.height/2;
-		//player.vy = -35;
-		//player.vy = -player.vy * .99;
-		//score = score + 1;
-		if(player.x < Platform.x - Platform.width/6)
-		{
-			player.vy = -35;
-			player.vx = -player.force*5;
-		}
-
-	}
-	if(player.hitTestObject(Platform))
-	{
-		if(player.x < Platform.x - Platform.width/6+41 && player.x > Platform.x - Platform.width/6)
-		{
-			player.vy = -35;
-			player.vx = -player.force;
-		}
-	}
-	if(player.hitTestObject(Platform))
-	{
-		if(player.x < Platform.x - Platform.width/6+80 && player.x > Platform.x - Platform.width/6+41)
-		{
-			player.vy = -35;
-		}
-	}
-	if(player.hitTestObject(Platform))
-	{
-		if(player.x < Platform.x - Platform.width/6+140 && player.x > Platform.x - Platform.width/6+80)
-		{
-			player.vy = -35;
-			player.vx = player.force;
-		}
-	}
-	if(player.hitTestObject(Platform))
-	{
-		if(player.x < Platform.x - Platform.width/6+215 && player.x > Platform.x - Platform.width/6+140)
-		{
-			player.vy = -35;
-			player.vx = player.force*5;
-		}
-	}
-	
-	
-
-	
-	player.drawCircle();
-	Platform.drawRect();
-	
+	player.drawRect();
 }
 
 
@@ -314,7 +173,6 @@ function showBounce()
 		player.vy += player.ay * player.force;
 	}
 	
-	
 	player.vy *= frictionY;
 	player.vx *= frictionX;
 	
@@ -331,21 +189,10 @@ function showBounce()
 		player.y = canvas.height - player.height/2;
 		//the decimal is how bouncy you want the object to be
 		//It should be a number between 0 and 2;
-		player.vy = -player.vy * .90;
-		score = 0;
-		
+		player.vy = -player.vy * .99;
 	}
-	if(player.hitTestObject(Platform))
-	{
-		//player.y = Platform.height - player.height/2;
-		player.vy = -35;
-		//player.vy = -player.vy * .99;
-		score = score + 1;
-	}
-
 	
 	//-----------------------------------------------------------------------------------------
 }
-
 
 
