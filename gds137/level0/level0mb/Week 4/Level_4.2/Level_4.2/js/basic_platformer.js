821a1cd303bcce083dff43f16ab7967c21013fae
Joshua Bholan <jbholan@email.neit.edu>
2023-05-28 18:47:13 -0400

    g

--- gds137/level0/level0mb/Week 4/Level_4.2/Level_4.2/js/basic_platformer.js
+++ gds137/level0/level0mb/Week 4/Level_4.2/Level_4.2/js/basic_platformer.js
@@ -112,14 +112,14 @@ function animate()
 				You simply need to change the values of the x and y properties of the object literals listed below to find the solution.
 	*/
 
-	while(platform0.hitTestPoint({x:player.x, y:player.y}) && player.vy >=0)
+	while(platform0.hitTestPoint({x:player.x-50, y:player.y+50}) && player.vy >=0)
 	{
 		player.y--;
 		player.vy = 0;
 		player.canJump = true;
 	}
 	
-	while(platform1.hitTestPoint({x:player.x, y:player.y}) && player.vy >=0)
+	while(platform1.hitTestPoint({x:player.x+50, y:player.y+50}) && player.vy >=0)
 	{
 		player.y--;
 		player.vy = 0;
