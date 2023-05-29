a343a35b68043465c6b1d76e54899b1539d58ebd
Joshua Bholan <jbholan@email.neit.edu>
2023-05-28 22:23:58 -0400

    nn

--- gds137/level0/level0mb/Week 4/Level_4.8/Level_4.8/Level_4.8/js/basic_platformer.js
+++ gds137/level0/level0mb/Week 4/Level_4.8/Level_4.8/Level_4.8/js/basic_platformer.js
@@ -116,21 +116,25 @@ function animate()
 		player.x--;
 		player.vx = 0;
 	}
-	while(platform1.hitTestPoint(player.top()) && player.vy <=0)
+	//while(platform1.hitTestPoint(player.top()) && player.vy <=0)
+	//{
+		//player.y++;
+		//player.vy = 0;
+	//}
+	//while(platform1.hitTestPoint(player.bottom()) && player.vy >=0)
+	//{
+		//player.y--;
+		//player.vy = 0;
+		//player.canJump = true;
+	//}
+	//while(platform1.hitTestPoint(player.left()) && player.vx <=0)
+	//{
+		//player.x++;
+		//player.vx = 0;
+	//}
+	if(platform1.hitTestPoint(player.right()) && player.vx >=0 && goal.y == 10000)
 	{
-		player.y++;
-		player.vy = 0;
-	}
-	while(platform1.hitTestPoint(player.bottom()) && player.vy >=0)
-	{
-		player.y--;
-		player.vy = 0;
-		player.canJump = true;
-	}
-	while(platform1.hitTestPoint(player.left()) && player.vx <=0)
-	{
-		player.x++;
-		player.vx = 0;
+		platform1.x = 2000;
 	}
 	while(platform1.hitTestPoint(player.right()) && player.vx >=0)
 	{
@@ -148,8 +152,6 @@ function animate()
 	{
 		goal.y = 10000;
 	}
-	
-	
 
 
 
